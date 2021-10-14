36cc1c253e037b49ef9635fc8aec80cdc1831c9b
Salih Özgör <imkb127@gmail.com>
2021-10-14 22:22:13 +0300

    New folder added

---
+++ While Foreach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace While_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz: ");
+            int sayı = int.Parse(Console.ReadLine());
+            int sayaç = 1;
+            int toplam = 0;
+            
+            while (sayaç<=sayı)
+            {
+                 toplam += sayaç;
+                 sayaç++;
+            }
+            Console.WriteLine(toplam/sayı);
+
+            char harf = 'a';
+
+            while (harf<='z')
+            {
+                 Console.Write(harf);
+                 harf++;
+            }
+
+
+            string[] arabalar = {"Audi","BMW","Mercedes","Honda"};
+            foreach (string araba in arabalar)
+            {
+                Console.Write("\n"+araba);
+            }
+        }
+    }
+}
