453bc2a95f6624bcf88cef56e2a4587667325e28
Salih Özgör <imkb127@gmail.com>
2021-10-15 15:37:27 +0300

    New folder added

---
+++ Array Sınıfı Methodları/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Array_Sınıfı_Methodları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayıdizisi = {5,9,1,4,6,44,27,11};
+            Console.WriteLine("Array sırasız");
+
+            foreach (var sayı in sayıdizisi)
+            {
+                Console.WriteLine(sayı);
+            }
+
+            Array.Sort(sayıdizisi);
+            Console.WriteLine("Array sıralı");
+
+            foreach (var sayı in sayıdizisi)
+            {
+                Console.WriteLine(sayı);
+            }
+
+            Array.Clear(sayıdizisi,2,2);
+            Console.WriteLine("Array Clear");
+
+            foreach (var sayı in sayıdizisi)
+            {
+                Console.WriteLine(sayı);
+            }
+
+            Array.Reverse(sayıdizisi);
+            Console.WriteLine("Array Reverse");
+
+            foreach (var sayı in sayıdizisi)
+            {
+                Console.WriteLine(sayı);
+            }
+
+            Console.WriteLine("Array IndexOf");
+            Console.WriteLine(Array.IndexOf(sayıdizisi,27));
+
+            Console.WriteLine("Array Resize");
+            Array.Resize<int>(ref sayıdizisi,9);
+            sayıdizisi[8] = 99;
+
+            foreach (var sayı in sayıdizisi)
+            {
+                Console.WriteLine(sayı);
+            }
+
+        }
+    }
+}
