00e3b79e980ee0363a7af1c65306f93ab274dc4f
Salih Özgör <imkb127@gmail.com>
2021-10-18 23:29:50 +0300

    New folder added

---
+++ Dictionary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int , string> kullanıcılar = new Dictionary<int, string>();
+            kullanıcılar.Add(10,"Ali");
+            kullanıcılar.Add(12,"Veli");
+            kullanıcılar.Add(8,"Mehmet");
+            kullanıcılar.Add(14,"Ayşe");
+
+
+            Console.WriteLine(kullanıcılar[12]);
+
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(kullanıcılar.Count);
+
+            Console.WriteLine(kullanıcılar.ContainsKey(12));
+            Console.WriteLine(kullanıcılar.ContainsValue("Salih"));
+            
+
+            kullanıcılar.Remove(8);
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in kullanıcılar.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in kullanıcılar.Values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Sınıf Nedir Instance_Field_Property/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Sınıf_Nedir_Instance_Field_Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+            calisan1.İsim = "Ayşe";
+            calisan1.Soyisim = "Karaali";
+            calisan1.No = 54216584;
+            calisan1.Departman = "İnsan Kaynakları";
+            calisan1.CalisanBilgileri();
+
+            Calisan calisan2 = new Calisan();
+            calisan2.İsim = "Muhammet";
+            calisan2.Soyisim = "Karaali";
+            calisan2.No = 61845324;
+            calisan2.Departman = "Halkla İlişkiler";
+            calisan2.CalisanBilgileri();
+
+
+        }
+    }
+
+    class Calisan
+    {
+        public string İsim;
+        public string Soyisim;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışanın ismi: {0}",İsim);
+            Console.WriteLine("Çalışanın soyismi: {0}",Soyisim);
+            Console.WriteLine("Çalışanın numarası: {0}",No);
+            Console.WriteLine("Çalışanın departmanı: {0}",Departman);
+        }
+    }
+}
