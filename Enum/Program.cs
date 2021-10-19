312e5d6a1d15015fc83619201a433697ebff1844
Salih Özgör <imkb127@gmail.com>
2021-10-19 14:33:24 +0300

    New folder added

---
+++ Enum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazartesi);
+            Console.WriteLine((int)Gunler.Cumartesi);
+            Console.WriteLine("Sıcaklık değerini giriniz: ");
+            int sicaklik = int.Parse(Console.ReadLine());
+
+            if (sicaklik < (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Hava Soğuk");
+            }
+            else if (sicaklik < (int)HavaDurumu.Sicak)
+            {
+                Console.WriteLine("Hava Ilık");
+            }
+            else if (sicaklik < (int)HavaDurumu.CokSicak)
+            {
+                Console.WriteLine("Hava Sıcak");
+            }
+            else
+            {
+                Console.WriteLine("Hava Çok Sıcak");
+            }
+
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 1, // default u 0 dır. yani sıfırdan saymaya  başlar.
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+}
---
+++ Struct(Yapı) Kavramı/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Struct_Yapı__Kavramı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+            Console.WriteLine(dikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_Struct; //new'lenmeden kullanılabilirler
+            dikdortgen_Struct.KisaKenar = 3;
+            dikdortgen_Struct.UzunKenar = 4;
+            Console.WriteLine(dikdortgen_Struct.AlanHesapla());
+            
+        }
+    }
+
+    class Dikdortgen
+    {
+        public Dikdortgen()
+        {
+            KisaKenar = 3;
+            UzunKenar = 4;
+        }
+        public int KisaKenar;
+        public int UzunKenar;
+        public long AlanHesapla()
+        {
+            return KisaKenar*UzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public Dikdortgen_Struct(int kisaKenar , int uzunKenar)   // parametresi olmayan constructor tanımlayamayız
+        {
+            KisaKenar = kisaKenar;
+            UzunKenar = uzunKenar;
+        }
+        public int KisaKenar;
+        public int UzunKenar;
+        public long AlanHesapla()
+        {
+            return KisaKenar*UzunKenar;
+        }
+    }
+}
